9969486ab297a726f42c830e128be8370df152a4
袁佳林 <572598394@qq.com>
2023-11-13 09:11:38 +0800

    登录主逻辑的开发

--- big-event/src/main/java/com/yjl/controller/UserController.java
+++ big-event/src/main/java/com/yjl/controller/UserController.java
@@ -3,7 +3,12 @@ package com.yjl.controller;
 import com.yjl.pojo.Result;
 import com.yjl.pojo.User;
 import com.yjl.service.UserService;
+
+
+import com.yjl.utils.Md5Util;
 import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  **/
 @RestController
+@Validated
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
     @PostMapping("/register")
-    public Result register(String userName,String  passWord){
-        User user=userService.findUserByName(userName);
-        if(user==null){
-            userService.register(userName,passWord);
+    public Result register(@Pattern(regexp = "^\\S{5,16}$")String userName, @Pattern(regexp = "^\\S{5,16}$")String passWord) {
+
+        User user = userService.findUserByName(userName);
+        if (user == null) {
+            userService.register(userName, passWord);
             return Result.success();
-        }else{
+        } else {
             return Result.error("用户名已经被使用");
         }
-
+    }
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$")String userName, @Pattern(regexp = "^\\S{5,16}$")String passWord){
+        User user = userService.findUserByName(userName);
+        if(user==null){
+            return Result.error("用户名不存在");
+        }
+        if(Md5Util.getMD5String(passWord).equals(user.getPassword())){
+            return Result.success("jwt token令牌..");
+        }
+        return Result.error("密码错误");
     }
 }
---
+++ big-event/src/main/java/com/yjl/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.yjl.exception;
+
+
+import com.yjl.pojo.Result;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Author: DLMU 袁佳林
+ * @Date: 2023/11/12 20:31
+ * @Description:
+ **/
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e){
+        e.printStackTrace();
+        return Result.error(StringUtils.hasLength(e.getMessage())? e.getMessage() : "操作失败");
+    }
+}
