54fd150becd31597afcaa207400117e2c7b8aa7f
袁佳林 <572598394@qq.com>
2023-12-02 21:29:33 +0800

    开始前端开发

---
+++ big-event/前端/big-event/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginVue from './views/Login.vue';
+</script>
+
+<template>
+<LoginVue/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ big-event/前端/big-event/src/api/user.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request.js'
+
+export const userRegisterService=(registerData)=>{
+    //借助于UrlSearchParams
+    new params=new URLSearchParams();
+    for(let key in registerData){
+        params.append(key,registerData[key]);
+
+    }
+    return request.post('/user/register',params);
+}
