f3a5303b006c2d2e1ea7df4053dd32a6b360aca6
袁佳林 <572598394@qq.com>
2023-12-13 20:50:07 +0800

    分类管理的开发

--- sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -95,5 +95,17 @@ public class EmployeeController {
         employeeService.startOrStop(status,id);
         return Result.success();
     }
-
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee>selectById(@PathVariable Long id){
+        Employee employee=employeeService.getById(id);
+        return Result.success(employee);
+    }
+    @PutMapping
+    @ApiOperation("编辑员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息：{}",employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
--- sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -25,4 +25,6 @@ public interface EmployeeMapper {
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     void update(Employee employee);
+    @Select("select * from employee where id=#{id}")
+    Employee getById(Long id);
 }
