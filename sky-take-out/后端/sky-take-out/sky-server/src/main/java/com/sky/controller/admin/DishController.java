6243dfeda694177004a8c45af22a642f21fad3f0
袁佳林 <572598394@qq.com>
2023-12-16 10:12:36 +0800

    菜品查询

--- sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,15 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: DLMU 袁佳林
@@ -28,5 +28,12 @@ public class DishController {
         log.info("新增菜品:{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
+    }
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult>page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询:{}",dishPageQueryDTO);
+        PageResult pageResult= dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-take-out/后端/sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
     public void saveWithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
